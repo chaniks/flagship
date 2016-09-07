972184848f599c74897688609748eb577db3b3bc
Chanik Yeon <chaniks@gmail.com>
2016-09-08 01:49:40 +0900

    file_spec: typo fixed

--- spec/file_spec.rb
+++ spec/file_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 require 'flagship'
 
 describe File do
-  describe 'in_files?' do
+  describe '#in_files?' do
     let(:file) { __FILE__ }
     it 'determines if file is in the list' do
       expr {
